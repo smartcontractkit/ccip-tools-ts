5e05901cd1e27ff2fca5d6e571850a0e043d2e19
André Vitor de Lima Matos <andre.matos@smartcontract.com>
2024-09-07 14:26:31 -0300

    collect coverage from tests

--- eslint.config.js
+++ eslint.config.js
@@ -41,6 +41,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
   {
