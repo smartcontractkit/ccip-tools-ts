52e58846e971501c1884624c646262ebea47dfe7
Andre Matos <andre.matos@smartcontract.com>
2025-12-03 07:39:16 -0500

    add sui chain hasher

--- ccip-cli/src/index.ts
+++ ccip-cli/src/index.ts
@@ -9,7 +9,7 @@ import { Format } from './commands/index.ts'
 util.inspect.defaultOptions.depth = 6 // print down to tokenAmounts in requests
 // generate:nofail
 // `const VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-const VERSION = '0.91.0-87e59c2'
+const VERSION = '0.90.2-e2dea99'
 // generate:end
 
 const globalOpts = {
