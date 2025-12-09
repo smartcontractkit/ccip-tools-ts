6273f1c78ed4a98ff783530e16c1a93b3e860d65
Andre Matos <andre.matos@smartcontract.com>
2025-12-09 13:28:09 -0500

    chore: bump deps, version to v0.92

--- ccip-cli/src/index.ts
+++ ccip-cli/src/index.ts
@@ -11,7 +11,7 @@ import { Format } from './commands/index.ts'
 util.inspect.defaultOptions.depth = 6 // print down to tokenAmounts in requests
 // generate:nofail
 // `const VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-const VERSION = '0.91.1-4e91371'
+const VERSION = '0.92.0-42fc5dd'
 // generate:end
 
 const globalOpts = {
