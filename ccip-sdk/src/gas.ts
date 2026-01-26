3181e0aa1ce2995592594a28a6c8a799ae692489
Andre Matos <andre.matos@smartcontract.com>
2026-01-26 12:07:20 -0500

    improve estimateReceiveExecution options
    receive object, simplify required parameters
    Follow-up to #114

--- ccip-sdk/src/gas.ts
+++ ccip-sdk/src/gas.ts
@@ -1,55 +1,88 @@
 import { type BytesLike, formatUnits, hexlify, randomBytes } from 'ethers'
 
 import type { Chain } from './chain.ts'
-import { CCIPMethodUnsupportedError, CCIPTokenDecimalsInsufficientError } from './errors/index.ts'
+import {
+  CCIPContractTypeInvalidError,
+  CCIPMethodUnsupportedError,
+  CCIPTokenDecimalsInsufficientError,
+} from './errors/index.ts'
 import { discoverOffRamp } from './execution.ts'
 import { sourceToDestTokenAddresses } from './requests.ts'
-import type { Lane } from './types.ts'
+
+/**
+ * A subset of [[MessageInput]] for estimating receive execution gas.
+ */
+export type EstimateMessageInput = {
+  /** receiver contract address */
+  receiver: string
+  /** optional messageId; random hash will be passed if omitted */
+  messageId?: string
+  /** optional sender: zero address will be used if omitted */
+  sender?: string
+  /** optional data: zero bytes will be used if ommitted */
+  data?: BytesLike
+  /**
+   * optional tokenAmounts; `amount` with either source `token` (as in MessageInput) or
+   * `{ sourceTokenAddress?, sourcePoolAddress, destTokenAddress }` (as in v1.5..v1.7 tokenAmounts)
+   * can be provided
+   */
+  tokenAmounts?: readonly ({
+    amount: bigint
+  } & (
+    | { token: string }
+    | { sourceTokenAddress?: string; sourcePoolAddress: string; destTokenAddress: string }
+  ))[]
+}
+
+/**
+ * Options for [[estimateReceiveExecution]] function.
+ */
+export type EstimateReceiveExecutionOpts = {
+  /** Source chain instance (for token data retrieval) */
+  source: Chain
+  /** Dest chain instance (for token and execution simulation) */
+  dest: Chain
+  /** source router or onRamp, or dest offRamp contract address */
+  routerOrRamp: string
+  /** message to be simulated */
+  message: EstimateMessageInput
+}
 
 /**
  * Estimate CCIP gasLimit needed to execute a request on a contract receiver.
- * @param source - Source chain object
- * @param dest - Dest chain object
- * @param request - CCIP request info containing `lane` and `message` details.
- *   `message` here is a subset of source-side [[CCIPMessage]] or [[MessageInput]], where either
- *   older `token` or newer `sourcePoolAddress`+`destTokenAddress` can be used for `tokenAmounts`
+ * @param opts - Options for estimation: source and dest chains, router or ramp address, and message
  * @returns Estimated gasLimit.
  */
-export async function estimateReceiveExecution(
-  source: Chain,
-  dest: Chain,
-  request: {
-    lane: Lane
-    message: {
-      messageId?: string
-      sender?: string
-      receiver: string
-      data?: BytesLike
-      tokenAmounts?: readonly ({
-        amount: bigint
-      } & (
-        | { token: string }
-        | { sourceTokenAddress?: string; sourcePoolAddress: string; destTokenAddress: string }
-      ))[]
-    }
-  },
-) {
+export async function estimateReceiveExecution({
+  source,
+  dest,
+  routerOrRamp,
+  message,
+}: EstimateReceiveExecutionOpts) {
   if (!dest.estimateReceiveExecution)
     throw new CCIPMethodUnsupportedError(dest.constructor.name, 'estimateReceiveExecution')
 
-  const offRamp = await discoverOffRamp(source, dest, request.lane.onRamp, source)
+  let onRamp, offRamp: string
+  try {
+    const tnv = await source.typeAndVersion(routerOrRamp)
+    if (!tnv[0].includes('OnRamp'))
+      onRamp = await source.getOnRampForRouter(routerOrRamp, dest.network.chainSelector)
+    else onRamp = routerOrRamp
+    offRamp = await discoverOffRamp(source, dest, onRamp, source)
+  } catch {
+    const tnv = await dest.typeAndVersion(routerOrRamp)
+    if (!tnv[0].includes('OffRamp'))
+      throw new CCIPContractTypeInvalidError(routerOrRamp, tnv[2], ['OffRamp'])
+    offRamp = routerOrRamp
+    onRamp = await dest.getOnRampForOffRamp(offRamp, source.network.chainSelector)
+  }
 
   const destTokenAmounts = await Promise.all(
-    (request.message.tokenAmounts ?? []).map(async (ta) => {
+    (message.tokenAmounts ?? []).map(async (ta) => {
       const tokenAmount =
         'destTokenAddress' in ta
           ? ta
-          : await sourceToDestTokenAddresses(
-              source,
-              dest.network.chainSelector,
-              request.lane.onRamp,
-              ta,
-            )
+          : await sourceToDestTokenAddresses(source, dest.network.chainSelector, onRamp, ta)
       const sourceTokenAddress =
         'token' in ta
           ? ta.token
@@ -73,13 +106,13 @@ export async function estimateReceiveExecution(
     }),
   )
   return dest.estimateReceiveExecution({
-    receiver: request.message.receiver,
+    receiver: message.receiver,
     offRamp,
     message: {
-      messageId: request.message.messageId ?? hexlify(randomBytes(32)),
-      sender: request.message.sender,
-      data: request.message.data,
-      sourceChainSelector: request.lane.sourceChainSelector,
+      messageId: message.messageId ?? hexlify(randomBytes(32)),
+      sender: message.sender,
+      data: message.data,
+      sourceChainSelector: source.network.chainSelector,
       destTokenAmounts,
     },
   })
