865178742d1ca6c35a716c28a520c70df5dbdf93
Andre Matos <andre.matos@smartcontract.com>
2026-02-25 09:27:15 -0500

    address comments

--- ccip-sdk/src/offchain.ts
+++ ccip-sdk/src/offchain.ts
@@ -1,4 +1,4 @@
-import { dataLength, dataSlice, getBytes, toNumber } from 'ethers'
+import { type BytesLike, dataLength, dataSlice, getBytes, toNumber } from 'ethers'
 import type { PickDeep } from 'type-fest'
 
 import {
@@ -11,8 +11,8 @@ import { type CCIPRequest, type OffchainTokenData, type WithLogger, NetworkType 
 import { networkInfo } from './utils.ts'
 
 const CIRCLE_API_URL = {
-  mainnet: 'https://iris-api.circle.com/v2',
-  testnet: 'https://iris-api-sandbox.circle.com/v2',
+  mainnet: 'https://iris-api.circle.com',
+  testnet: 'https://iris-api-sandbox.circle.com',
 }
 
 type CctpAttestationResponse =
@@ -28,9 +28,6 @@ type CctpAttestationResponse =
 
 /**
  * Returns the USDC attestation for a given tokenAmount.extraData and txHash
- * USDCTokenPool's extraData is a packed `SourceTokenDataPayloadV1{uint64 nonce, uint32 sourceDomain}`,
- * which we need to query CCTPv2 (by sourceDomain and txHash) and to filter by nonce among messages,
- * if more than one
  * https://developers.circle.com/cctp/quickstarts/transfer-usdc-ethereum-to-arc#3-3-retrieve-attestation
  *
  * @param opts - CCTPv2 options
@@ -51,7 +48,9 @@ export async function getUsdcAttestation(
   const { sourceDomain, nonce, txHash } = opts
   const circleApiBaseUrl =
     networkType === NetworkType.Mainnet ? CIRCLE_API_URL.mainnet : CIRCLE_API_URL.testnet
-  const res = await fetch(`${circleApiBaseUrl}/messages/${sourceDomain}?transactionHash=${txHash}`)
+  const res = await fetch(
+    `${circleApiBaseUrl}/v2/messages/${sourceDomain}?transactionHash=${txHash}`,
+  )
   const json = (await res.json()) as CctpAttestationResponse
   let att
   if ('messages' in json) {
@@ -122,6 +121,31 @@ export async function getOffchainTokenData(
 ): Promise<OffchainTokenData[]> {
   const { networkType } = networkInfo(request.message.sourceChainSelector)
 
+  function looksUsdcData(extraData: BytesLike) {
+    if (dataLength(extraData) !== 64) return
+    // USDCTokenPool's extraData is a packed `SourceTokenDataPayloadV1{uint64 nonce, uint32 sourceDomain}`,
+    // which we need to query CCTPv2 (by sourceDomain and txHash) and to filter by nonce among messages,
+    // if more than one in tx
+    let nonce, sourceDomain
+    try {
+      // those toNumber conversions throw early in case the bytearray don't look like small numbers
+      nonce = toNumber(dataSlice(extraData, 0, 32))
+      sourceDomain = toNumber(dataSlice(extraData, 32, 32 + 32))
+      return { nonce, sourceDomain } // maybe USDC
+    } catch {
+      // not USDC
+    }
+  }
+
+  function looksLbtcData(extraData: BytesLike) {
+    // LBTC returns `message_hash`/`payloadHash` directly as `bytes32 extraData`
+    if (
+      dataLength(extraData) === 32 &&
+      getBytes(extraData, 'extraData').filter(Boolean).length > 20 // looks like a hash
+    )
+      return true
+  }
+
   return Promise.all(
     request.message.tokenAmounts.map(async (tokenAmount, i) => {
       let extraData
@@ -136,38 +160,22 @@ export async function getOffchainTokenData(
         }
       }
       if (!extraData) return
-      if (dataLength(extraData) === 64) {
-        let nonce, sourceDomain
+      const usdcOpts = looksUsdcData(extraData)
+      if (usdcOpts) {
         try {
-          // those toNumber conversions throw early in case the bytearray don't look like small numbers
-          nonce = toNumber(dataSlice(extraData, 0, 32))
-          sourceDomain = toNumber(dataSlice(extraData, 32, 32 + 32))
-        } catch {
-          return
-        }
-        try {
-          return {
-            _tag: 'usdc',
-            extraData,
-            ...(await getUsdcAttestation(
-              { sourceDomain, nonce, txHash: request.tx.hash },
-              networkType,
-            )),
-          }
+          const usdcAttestation = await getUsdcAttestation(
+            { ...usdcOpts, txHash: request.tx.hash },
+            networkType,
+          )
+          return { _tag: 'usdc', extraData, ...usdcAttestation }
         } catch (err) {
           // maybe not a USDC transfer, or not ready
           logger.warn(`❌ CCTP: Failed to fetch attestation for message:`, request.message, err)
         }
-      } else if (
-        dataLength(extraData) === 32 &&
-        getBytes(extraData, 'extraData').filter(Boolean).length > 20 // looks like a hash
-      ) {
+      } else if (looksLbtcData(extraData)) {
         try {
-          return {
-            _tag: 'lbtc',
-            extraData,
-            ...(await getLbtcAttestation(extraData, networkType)),
-          }
+          const lbtcAttestation = await getLbtcAttestation(extraData, networkType)
+          return { _tag: 'lbtc', extraData, ...lbtcAttestation }
         } catch (err) {
           logger.warn(`❌ LBTC: Failed to fetch attestation for message:`, extraData, err)
         }
