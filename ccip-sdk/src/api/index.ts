1b8bf1a73674827752a85b064cc5b4b3f5459286
Andre Matos <andre.matos@smartcontract.com>
2026-03-11 15:34:03 -0400

    chore: upgrade minor deps, bump version to v1.2.0

--- ccip-sdk/src/api/index.ts
+++ ccip-sdk/src/api/index.ts
@@ -62,7 +62,7 @@ export const DEFAULT_TIMEOUT_MS = 30000
 /** SDK version string for telemetry header */
 // generate:nofail
 // `export const SDK_VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-export const SDK_VERSION = '1.1.1-495088e'
+export const SDK_VERSION = '1.2.0-e822ab8'
 // generate:end
 
 /** SDK telemetry header name */
