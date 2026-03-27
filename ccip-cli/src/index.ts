e31c07e7040372811620ac1e933f286c7359def4
André Vitor de Lima Matos <andre.matos@smartcontract.com>
2026-03-27 09:12:09 -0400

    chore: update deps, prepare v1.4.0 release (#209)

--- ccip-cli/src/index.ts
+++ ccip-cli/src/index.ts
@@ -11,7 +11,7 @@ import { Format } from './commands/index.ts'
 util.inspect.defaultOptions.depth = 6 // print down to tokenAmounts in requests
 // generate:nofail
 // `const VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-const VERSION = '1.3.1-480daa1'
+const VERSION = '1.4.0-62b220c'
 // generate:end
 
 const globalOpts = {
