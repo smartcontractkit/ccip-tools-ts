07b40cb500ca712236cfdbae2d95585c388acaed
aelmanaa <aelmanaa@gmail.com>
2026-04-07 23:11:32 +0100

    feat(sdk): add getMessagesInRange for block/slot-based CCIP message discovery

--- ccip-cli/src/index.ts
+++ ccip-cli/src/index.ts
@@ -11,7 +11,7 @@ import { Format } from './commands/index.ts'
 util.inspect.defaultOptions.depth = 6 // print down to tokenAmounts in requests
 // generate:nofail
 // `const VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-const VERSION = '1.4.2-7f8e132'
+const VERSION = '1.4.2-f874d03'
 // generate:end
 
 const globalOpts = {
