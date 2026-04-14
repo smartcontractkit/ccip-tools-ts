751cb9971193c125d4964bda74ce3ba35c0f97b8
Ryan Nelson <ryan.nelson@smartcontract.com>
2026-04-14 12:30:16 -0400

    Installed dependencies

--- ccip-sdk/src/api/index.ts
+++ ccip-sdk/src/api/index.ts
@@ -60,7 +60,7 @@ export const DEFAULT_TIMEOUT_MS = 30000
 /** SDK version string for telemetry header */
 // generate:nofail
 // `export const SDK_VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-export const SDK_VERSION = '1.4.2-851fdcb'
+export const SDK_VERSION = '1.4.2-66391dc'
 // generate:end
 
 /** SDK telemetry header name */
