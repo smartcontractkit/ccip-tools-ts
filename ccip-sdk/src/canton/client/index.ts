d86c4816af8f55f0baa2f27b65a68ff4118b376d
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2026-04-23 12:06:05 +0200

    add canton sdk send + execute

---
+++ ccip-sdk/src/canton/client/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Canton Ledger API - Public exports
+ */
+
+export {
+  type CantonClient,
+  type CantonClientConfig,
+  type Command,
+  type ConnectedSynchronizer,
+  type CreatedEvent,
+  type EventFormat,
+  type GetActiveContractsRequest,
+  type GetConnectedSynchronizersResponse,
+  type JsActiveContract,
+  type JsCantonError,
+  type JsCommands,
+  type JsGetActiveContractsResponse,
+  type JsSubmitAndWaitForTransactionResponse,
+  type JsTransaction,
+  type SubmitAndWaitResponse,
+  type TemplateFilter,
+  type TransactionFilter,
+  type WildcardFilter,
+  createCantonClient,
+} from './client.ts'
