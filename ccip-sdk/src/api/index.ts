b24fef7f8b8636773889a4b6b9e85ca53313ddc7
André Vitor de Lima Matos <andre.matos@smartcontract.com>
2026-05-28 08:29:35 -0400

    chore: bump version to 1.7.1

--- ccip-sdk/src/api/index.ts
+++ ccip-sdk/src/api/index.ts
@@ -58,7 +58,7 @@ export const DEFAULT_TIMEOUT_MS = 30000
 /** SDK version string for telemetry header */
 // generate:nofail
 // `export const SDK_VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-export const SDK_VERSION = '1.7.0-636a22c'
+export const SDK_VERSION = '1.7.1-90c0438'
 // generate:end
 
 /** SDK telemetry header name */
