300a1866b2f063e96f25997dd497e99e05541876
SyedAsadKazmi <syed.asadkazmi@smartcontract.com>
2026-06-26 21:02:56 +0530

    chore: regenerate VERSION and SDK_VERSION after rebase

--- ccip-cli/src/index.ts
+++ ccip-cli/src/index.ts
@@ -28,7 +28,7 @@ Error.stackTraceLimit = 50 // show more stack frames for better debugging
 
 // generate:nofail
 // `const VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-const VERSION = '1.9.2-ae443e7'
+const VERSION = '1.9.2-9e31827'
 // generate:end
 
 const require = createRequire(import.meta.url)
