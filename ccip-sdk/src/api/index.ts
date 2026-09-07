f652d7fc8df7b28c107fba66eb641447c2ea4815
mervin-link <tvc-mervin.villaceran@smartcontract.com>
2026-09-08 00:52:11 +0800

    fix: lint errors

--- ccip-sdk/src/api/index.ts
+++ ccip-sdk/src/api/index.ts
@@ -63,7 +63,7 @@ export const DEFAULT_TIMEOUT_MS = 30000
 /** SDK version string for telemetry header */
 // generate:nofail
 // `export const SDK_VERSION = '${require('./package.json').version}-${require('child_process').execSync('git rev-parse --short HEAD').toString().trim()}'`
-export const SDK_VERSION = '1.13.1-e6a58224'
+export const SDK_VERSION = '1.13.1-48759b5c'
 // generate:end
 
 /** SDK telemetry header name */
